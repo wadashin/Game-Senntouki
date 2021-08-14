5e73bec3c91e975feafc83f821b25172d29a7b73
wadashin <Shin.86.miso@outlook.com>
2021-08-14 23:51:06 +0900

    操作の確定とUAVの生成を追加した

---
+++ Assets/Script/UAVBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UAVBase : MonoBehaviour
+{
+    [SerializeField] float m_Speed = 10;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = gameObject.transform.rotation * new Vector3(0, 0, m_Speed);
+        gameObject.transform.position += velocity;
+
+        UAV();
+    }
+
+    public abstract void UAV();
+}
+
---
+++ Assets/Script/UAVCreate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UAVCreate : MonoBehaviour
+{
+    [SerializeField] GameObject UAV;
+    // Update is called once per frame
+    private void Start()
+    {
+        Create();
+    }
+
+    void Update()
+    {
+
+        
+    }
+    public void Create()
+    {
+    Vector3 play1 = this.gameObject.transform.position;
+    Quaternion play2 = this.gameObject.transform.rotation;
+
+        Debug.Log("play: " + play1);
+
+    Instantiate(UAV, play1, play2);
+    }
+}
